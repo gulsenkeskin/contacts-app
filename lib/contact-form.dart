945eebb49bb7bdfadc1ebe9313ca10d132d74957
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-29 19:08:53 +0300

    Create contact-form.dart

---
+++ lib/contact-form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatefulWidget {
+  const ContactForm({Key? key}) : super(key: key);
+
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
