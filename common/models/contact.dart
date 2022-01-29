9b3abd87fc37c0cf236327bc5c3654c3e5920b7e
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-29 18:55:15 +0300

    Create contact.dart

---
+++ common/models/contact.dart
@@ -0,0 +1,13 @@
+class Contact {
+  final String firstName;
+  final String lastName;
+  final String imageUrl;
+  final String phone;
+
+  Contact({
+    required this.firstName,
+    required this.lastName,
+    required this.imageUrl,
+    required this.phone,
+  });
+}
